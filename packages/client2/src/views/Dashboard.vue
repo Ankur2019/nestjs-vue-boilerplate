8c93337394cd59f9d3949d7eb8ffd24c100bb4af
Ankur2019 <ankur@nerddevs.com>
2022-01-28 14:46:26 +0600

    auth module of vuex added

---
+++ packages/client2/src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{headline}}</h1>
+    <div>Hi there, {{ $store.state.auth.user.name }}</div>
+
+    <div>
+      Thanks for using the NestJS Vue Boilerplate. I really hope you enjoy it so
+      far!
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'Dashboard',
+  data() {
+    return {
+      headline: 'Dashboard',
+    };
+  },
+});
+</script>
